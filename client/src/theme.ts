df337fa4d1f331701dfc92667c3c7a59a47eef8c
Naveen-works <naveenkarthik26042005@gmail.com>
2025-04-04 19:36:29 +0530

    Initialization of routes and Navbar creation

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -36,7 +36,19 @@ export const tokens = {
     },
     tertiary: {
       // purple
-      500: "#8884d8",
+        light: "#A3BFFA",
+        main: "#3B82F6",
+        dark: "#1E40AF",
+        contrastText: "#ffffff",
+        100: "#DBEAFE",
+        200: "#BFDBFE",
+        300: "#93C5FD",
+        400: "#60A5FA",
+        500: "#3B82F6",
+        600: "#2563EB",
+        700: "#1D4ED8",
+        800: "#1E40AF",
+        900: "#1E3A8A"
     },
     background: {
       light: "#2d2d34",
