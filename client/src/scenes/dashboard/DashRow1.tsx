a52fe41183c2c09f08a730b8a33cca8f9f179317
Naveen-works <naveenkarthik26042005@gmail.com>
2025-04-12 17:16:15 +0530

    Inital line area chart data and types.ts build

--- client/src/scenes/dashboard/DashRow1.tsx
+++ client/src/scenes/dashboard/DashRow1.tsx
@@ -1,15 +1,59 @@
-import DashboardBox from '@/components/DashboardBox'
-//import { useGetKpisQuery } from '@/state/api'
+import DashboardBox from '@/components/DashboardBox';
+import { useGetKpisQuery } from '@/state/api';
+import { useTheme } from '@mui/material';
+import { useMemo } from 'react';
+import {
+  ResponsiveContainer,
+  AreaChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Area,
+} from 'recharts';
 
 const DashRow1 = () => {
-    //const {data} =useGetKpisQuery(); 
-    return (
-        <>
-         <DashboardBox   gridArea="a"></DashboardBox>
-        <DashboardBox   gridArea="b"></DashboardBox>
-        <DashboardBox   gridArea="c"></DashboardBox>
-        </>
-    )
-}
+  const { data } = useGetKpisQuery();
+  const {palette} =useTheme();
+  console.groupCollapsed("Data :" , data);
+  
+  const revenueExpenses = useMemo(() => {
+    if (!data || !data[0]) return [];
 
-export default DashRow1
+    return data[0].monthlyData.map(({ month, revenue, expenses }) => ({
+      name: month.substring(0, 3),
+      revenue,
+      expenses,
+    }));
+  }, [data]);
+
+  return (
+    <>
+      <DashboardBox gridArea="a">
+      <ResponsiveContainer width="100%" height="100%">
+        <AreaChart
+          width={500}
+          height={400}
+          data={revenueExpenses}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="revenue" stroke={palette.primary.main} fillOpacity={1} fill="url(colorRevenue)" />
+        </AreaChart>
+      </ResponsiveContainer>
+      </DashboardBox>
+      <DashboardBox gridArea="b"></DashboardBox>
+      <DashboardBox gridArea="c"></DashboardBox>
+    </>
+  );
+};
+
+export default DashRow1;
